4692d4a97a200c7254be62d6b4307767bc96c49c
Nafseer Kadiyaravida <nafseerk@gmail.com>
2017-11-22 21:44:04 -0500

    Added DecisionTreeNode class

---
+++ decision_tree.py
@@ -0,0 +1,14 @@
+
+class DecisionTree(object):
+
+    def __init__(self, root_node):
+        self.__root = root_node
+
+
+class DecisionTreeLearning(object):
+
+    def learn(self):
+        pass
+
+    def predict(self):
+        pass
